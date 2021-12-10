1e45d0f2a3ec7cdfd93f821916428985b74a8e2e
Gary Zhu <gary.zhu@mercury.co.nz>
2021-12-10 15:21:44 +1300

    updates brew formula

--- aws-one-punch.rb
+++ aws-one-punch.rb
@@ -3,31 +3,15 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class AwsOnePunch < Formula
   desc "One punch to grant all command line AWS access"
-  homepage ""
-  url "https://github.com/GaaraZhu/aws-one-punch/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "34dffad82ccffe2d7934d7e2ce9fbc76e15a9834139b5b2f52754c711257b7ca"
-  license ""
+  homepage "https://github.com/GaaraZhu/aws-one-punch"
+  url "https://github.com/GaaraZhu/aws-one-punch/releases/download/v1.0.0/aws-one-punch"
+  sha256 "842a372102e423c70e2c7402fd01aecfc8b01177c8216530ceeba598427ad882"
+  license "MIT"
 
   # depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "aws-one-punch" 
   end
 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test aws-one-punch`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
-  end
 end
