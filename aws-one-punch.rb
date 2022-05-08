d6c95925d24ddf372c7768dc8dd0fc444eeae416
Gary Zhu <GaaraZhu@users.noreply.github.com>
2022-05-08 16:39:23 +1200

    Update aws-one-punch.rb

--- aws-one-punch.rb
+++ aws-one-punch.rb
@@ -5,7 +5,7 @@ class AwsOnePunch < Formula
   desc "One punch to grant all command line AWS access"
   homepage "https://github.com/GaaraZhu/aws-one-punch"
   url "https://github.com/GaaraZhu/aws-one-punch/releases/download/v2.0.0/aws-one-punch"
-  sha256 "67c6e72783d0886075fdd7a77422496ec35e2d61d889a9df46ce89adc95d031a"
+  sha256 "852111eb48921a17aab1c5991624f7c2a882816575ee7291651d2fba44b09a6a"
   license "MIT"
 
   # depends_on "cmake" => :build
